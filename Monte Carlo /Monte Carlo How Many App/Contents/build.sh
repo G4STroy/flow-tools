77eba4b51abdf5820712a6f6caa2dcb01bea7879
G4STroy <87447965+G4STroy@users.noreply.github.com>
2024-04-06 20:18:05 -0400

    completed MC How Many Mac App

--- Monte Carlo /Monte Carlo How Many App/Contents/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run PyInstaller
-pyinstaller MCHowManyapp.spec
-
-# Copy resources to the correct location in the app bundle
-cp -R "/Users/troy.lightfoot/Github Projects/flow-tools/Monte Carlo /Monte Carlo How Many App/Contents/Resources/" "dist/MCHowManyapp.app/Contents/Resources/"
